df339b1e430ba9592dc067316812a968353890bb
Maria-camila0218 <maricamilagaleano69@gmail.com>
2020-09-30 23:30:44 -0500

    Ajustes

--- frontend/src/atoms/footer.jsx
+++ frontend/src/atoms/footer.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//space
+
 function foot() {
     return (
         <div className="down">
             <h1 id="cers">Contáctanos en redes sociales</h1>
             <Link to="/Políticas"><h3>Políticas y condiciones</h3></Link>
+            <div className="socialR">
+                <p>Encuentranos también en:</p>
+                <img src="" alt="Google"/>
+                <img src="" alt="Facebook"/>
+            </div>
         </div>
     )
 }
