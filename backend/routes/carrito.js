031477acd6b18bcf27f15ef7cbec4f9a844561b6
Maria-camila0218 <camilagaleanom69@gmail.com>
2020-11-17 15:21:26 -0500

    carrito_mariacamila

---
+++ backend/routes/carrito.js
@@ -0,0 +1,52 @@
+const {Router}=require('express');
+const router=Router();
+const mysqlconnection=require('../DB/DB');
+//Search
+router.get('/carrito:nombre', (req,res) => {
+    mysqlconnection.query('SELECT * FROM carrito WHERE nombre=?', (err, rows, fields) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json(rows);
+        }
+    });
+});
+//Create
+router.post('/carrito', (req, res) => {
+    const {nombre_carrito, nombre_productos, productos};
+    let productos=[nombre_carrito, nombre_productos, productos];
+    let queryCarrito='INSERT INTO carrito(nombre_carrito, nombre_productos, productos) VALUES(?,?,?)';
+    mysqlconnection.query(productos, queryCarrito, (err, rows, fields) => {
+        if(!err){
+            console.log(err.message);
+        }else{
+            res.json({status: 'Creación exitosa'});
+        }
+    });
+});
+//Delete
+router.delete('/carrito:codigo_carrito', (req, res) =>{
+    const {codigo_carrito}=req.params;
+    mysqlconnection.query('DELETE FROM carrito WHERE codigo_carrito=?',
+    [codigo_carito], (err, rows, fields)=>{
+        if(err){
+            console.log(err.message);
+        }else{
+            res.json({status: 'Carrito eliminado con exito'});
+        }
+    });
+});
+//Change
+router.put('/carrito:codigo_carrito', (req, res) => {
+    const {nombre_carrito, nombre_productos, productos}=req.body;
+    const {codigo_carrito}=req.params;
+    mysqlconnection.query('UPDATE carrito SET nombre_carrito=?, nombre_productos=?, productos=?',
+    [nombre_carrito, nombre_productos, productos], (err, rows, fields)=>{
+        if(err){
+            console.log(err.message);
+        }else{
+            res.json({status: 'Actualizacion exitosa'});
+        }
+    });
+});
+module.exports=mysqlconnection;
