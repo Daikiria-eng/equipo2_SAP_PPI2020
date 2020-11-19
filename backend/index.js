03cc4b403d03fa0dec25925fef6f7470f55d0f6a
Daikiria-eng <blackincolornegro@gmail.com>
2020-11-19 22:26:51 +0000

    Uso de postman

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 const express=require('express');
 const app=express();
-const routes=require('./routes/users');
+const routes=require('./routes');
+const users=require('.routes/users');
+const grocery=require('routes/abarrote');
+const domiciliary=require('routes/domicliario');
+const product=require('routes/producto');
+const car=require('routes/carrito');
 const port=process.env.PORT || 4001;
 app.use(express.json());
 app.use('/rank', routes);
+app.use('/usuarios', users);
+app.use('/abarrote', grocery);
+app.use('/domiciliario', domiciliary);
+app.use('/producto', product);
+app.use('/carrito', car);
 app.listen(app.get('port'), () => {
     console.log(`Running on port ${port}`);
 });
